af3d7a8b921943821aaa209c5d50b3c67a59ade2
BaptisteJost <jost.baptiste@gmail.com>
2020-08-27 08:28:41 +0200

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='jost@apc.in2p3.fr',
     packages=['bjlib'],
     install_requires=['numpy', 'healpy', 'pysm'],
-    version='0.0.2.3',
+    version='0.0.2.4',
     license='MIT',
     description='My personal library',
 )
