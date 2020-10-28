lib_project : cl_rotation_derivative() ERROR in sign !! fixed but might cause trouble in uses of the function later Likelihood_SO : addinc LiteBIRD option for frequencies and sensitivity

bjlib/cl_lib.py
@@ -96,7 +96,7 @@ def error_68(x_grid, y_grid, x_max_likelihood=0):
 
 
 def L_gridding(spectra_model, data_matrix, alpha_grid,
-               bin, nside, f_sky_SAT, spectra_used='all', return_m2logL=False):
+               bin, nside, f_sky_SAT, spectra_used='all', return_mlogL=False):
 
     grid_logL = []
     # grid_model_matrix = []
@@ -124,7 +124,7 @@ def L_gridding(spectra_model, data_matrix, alpha_grid,
     in_exp = -np.array(grid_logL) - max_logL  # max(-np.array(grid_logL))
 
     # , np.array(grid_model_matrix), np.array(grid_data_matrix), np.array(grid_Cm1D), np.array(likelihood_element_grid), (grid_alpha2, np.exp(in_exp_2))
-    if return_m2logL:
+    if return_mlogL:
         return np.exp(in_exp), grid_logL
     return np.exp(in_exp)
 

bjlib/lib_project.py
@@ -135,6 +135,8 @@ def cl_rotation_derivative(cl, angle_in):
         i.e. : TT, EE, BB, TE, EB, TB
 
     """
+    print('Warning ! cl_rotation_derivative() is an outdated version, sign error may occur')
+    print('Be particularly cautious about the use of TB,TE terms')
     angle = angle_in.to(u.rad).value
     cl_EE_rot_da = np.sin(4 * angle) * (cl[:, 1] - cl[:, 2])
     cl_BB_rot_da = np.sin(4 * angle) * (-cl[:, 1] + cl[:, 2])
@@ -150,9 +152,9 @@ def cl_rotation_derivative(cl, angle_in):
         cl_TE_rot_da += cl[:, 5] * np.cos(2 * angle)
         cl_TB_rot_da += cl[:, 5] * np.sin(2 * angle)
 
-    cl_rot_da = np.array([np.array([0, -2 * cl_EE_rot_da[k],
-                                    -2 * cl_BB_rot_da[k], -2 * cl_TE_rot_da[k],
-                                    -2 * cl_EB_rot_da[k],
+    cl_rot_da = np.array([np.array([0, 2 * cl_EE_rot_da[k],
+                                    2 * cl_BB_rot_da[k], -2 * cl_TE_rot_da[k],
+                                    2 * cl_EB_rot_da[k],
                                     -2 * cl_TB_rot_da[k]])
                           for k in range(len(cl))])
     return cl_rot_da