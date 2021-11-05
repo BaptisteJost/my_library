lib_project : error sign in EB power spectra corrected to be consistent with miscal. likelihood SO : error in if, del sky to free memory

bjlib/lib_project.py
@@ -91,7 +91,7 @@ def cl_rotation(cl, angle_in):
         (np.sin(2 * angle)**2)
     cl_BB_rot = cl[:, 1] * (np.sin(2 * angle)**2) + cl[:, 2] * \
         (np.cos(2 * angle)**2)
-    cl_EB_rot = (cl[:, 1] - cl[:, 2]) * np.sin(4 * angle) * 0.5
+    cl_EB_rot = (-cl[:, 1] + cl[:, 2]) * np.sin(4 * angle) * 0.5
     cl_TE_rot = cl[:, 3] * np.cos(2 * angle)
     cl_TB_rot = cl[:, 3] * np.sin(2 * angle)
     if cl.shape[0] == 6:
@@ -139,7 +139,7 @@ def cl_rotation_derivative(cl, angle_in):
     angle = angle_in.to(u.rad).value
     cl_EE_rot_da = np.sin(4 * angle) * (cl[:, 1] - cl[:, 2])
     cl_BB_rot_da = np.sin(4 * angle) * (-cl[:, 1] + cl[:, 2])
-    cl_EB_rot_da = np.cos(4 * angle) * (-cl[:, 1] + cl[:, 2])
+    cl_EB_rot_da = np.cos(4 * angle) * (cl[:, 1] - cl[:, 2])
     cl_TE_rot_da = cl[:, 3] * np.sin(2 * angle)
     cl_TB_rot_da = cl[:, 3] * (-np.cos(2 * angle))
 